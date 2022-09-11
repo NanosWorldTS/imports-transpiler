bea82210ae15c71457b7973d85e7c8bcc593270e
DKFN <valere.tetelin@gmail.com>
2022-09-11 16:46:41 +0200

    Fixes issue #1 and handles index imports

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
-
 import * as ts from "typescript";
 import * as tstl from "typescript-to-lua";
+import * as path from "node:path";
+import * as fs from "node:fs";
 import {VariableDeclarationStatement} from "typescript-to-lua";
+
 //@ts-ignore
 console.log("Imports transpiller starting");
 
@@ -28,16 +30,24 @@ const plugin: tstl.Plugin = {
 
             ++importMapsCount;
             const importMapName = "____importmap_"+importMapsCount;
-            // TODO: Handle external Lua package with type definitions
-            // TODO: Handle index.ts being stripped from the import path
 
-            const targetModulePath = identifier + ".lua";
+            // Tries to import the file directly, if it is not found its probably an import from an "index.ts"
+            const importerFile = (node as any)?.parent.fileName;
+            let targetModulePath;
+            const importerDir = path.dirname(importerFile);
+            const directSourceFilePath = path.format({dir: importerDir, base: identifier + ".ts"});
+            try {
+                fs.statSync(directSourceFilePath)
+                targetModulePath = identifier + ".lua";
+            } catch(e) {
+                targetModulePath = identifier + "/index.lua";
+            }
 
             // List all the imports done
             const defaultBinding = node.importClause?.name;
             const namedBindings = (node.importClause?.namedBindings as any)?.elements
 
-            // Build internal transpiller representation
+            // Build internal transpiller representation and sort default import and named imports
             const nodeRepr: IModuleImportNode = {
                 defaultImport: defaultBinding ? {
                     exportedName: 'default',
@@ -58,7 +68,7 @@ const plugin: tstl.Plugin = {
             // Package.Require Node
             const packageRequireAssignToImportMap = tstl.createVariableDeclarationStatement(tstl.createIdentifier(importMapName), tstl.createIdentifier(`Package.Require("${targetModulePath}")`))
 
-            // Named imports
+            // Imports
             const finalNamedImportsVariables: VariableDeclarationStatement[] = [...nodeRepr.namedImports ?? [], nodeRepr.defaultImport].map((moduleImportClause) => {
                 // console.log("Processing : ", moduleImportClause)
                 if (!moduleImportClause) return;
